7a7a8cb778268337d20c95db49d023c5bba1333d
Niamat Marjan <86093827+NimBuzz01@users.noreply.github.com>
2023-05-08 19:31:02 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Box, Container } from "@mui/material";
 import Header from "./components/header/Header";
+import Footer from "./components/header/footer/Footer";
 
 const App = () => {
   return (
-    <>
-      <Header />      
-    </>
+    <Box sx={{display:"flex", flexDirection:"column", height:"100vh"}}>
+      <Header />
+      <Container sx={{flexGrow:1}}></Container>
+      <Footer />
+    </Box>
   );
 };
 
