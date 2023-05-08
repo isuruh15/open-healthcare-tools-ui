9a89bf3ff8ce91e1f275a999a22e3d92181d8e08
Niamat Marjan <86093827+NimBuzz01@users.noreply.github.com>
2023-05-08 15:00:57 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Typography color="secondary.main" variant="h1">
+      Open Healthcare Sandbox V3
+    </Typography>
+  );
+};
+
+export default App;
---
+++ src/assets/app-theme.tsx
@@ -0,0 +1,49 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#A5A5A5", // grey
+      main: "#FFFFFF", // white
+      dark: "#222228", // black
+    },
+    secondary: {
+      light: "212A3E", // light black
+      main: "#5567D5", // blue
+      dark: "#00255C", // dark blue
+    },
+    success: {
+      main: "#34B474", // green
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 40,
+    },
+    h2: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 32,
+    },
+    h3: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h4: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 20,
+    },
+    h5: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 16,
+    },
+    h6: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+      fontSize: 14,
+    },
+  },
+});
+
+export default theme;
