c578c34ff72b9d44b8bc8aa17620cc3aa578eace
isuruh15 <isuruhemantha.15@cse.mrt.ac.lk>
2023-10-03 16:15:35 +0530

    modify wso2 blade

--- src/components/Common/Wso2Blade.tsx
+++ src/components/Common/Wso2Blade.tsx
@@ -12,34 +12,29 @@ interface Wso2BladeProps {
 
 export const Banner = ({ solution, language }: Wso2BladeProps) => {
     return (
-        <Box>
+        <Box sx={{
+            minHeight: 300,
+            flexDirection: "row",
+            display: "flex",
+        }}>
             <Box
                 sx={{
-                    backgroundImage: "url('https://ngspl.com/wp-content/uploads/2017/03/background-check-process.jpg-1.jpg')",
-                    backgroundBlendMode: "multiply",
-                    position: "relative",
                     minHeight: 300,
-                    alignItems: "center",
-                    justifyContent: "center",
+                    backgroundColor: "#00A79D60",
+                    backdropFilter: "blur(5px)",
+                    flexDirection: "column",
+                    display: "flex",
+                    padding:5,
+                    width:"0.5"
                 }}
             >
-                <Box
-                    sx={{
-                        minHeight: 300,
-                        backgroundColor: "#00A79D60",
-                        backdropFilter: "blur(5px)",
-                        flexDirection: "row",
-                        display: "flex",
-                    }}
-                    padding={5}
-                >
-                    <Grid container alignItems="center" justifyContent="center" marginLeft={6}>
-                    <Grid container alignItems="center" justifyContent="center" paddingTop={6} paddingLeft={5} paddingBottom={2}>
+                <Grid container alignItems="center" justifyContent="center">
+                    <Grid container alignItems="center" justifyContent="center" padding={5}>
                         <Typography variant="h2" align="center" color="#00255C" maxWidth="lg">
                             {solution.title}
                         </Typography>
                     </Grid>
-                    <Grid container alignItems="center" justifyContent="center" paddingLeft={5} >
+                    <Grid container alignItems="center" justifyContent="center" padding={2} >
                         <Typography
                             variant="body1"
                             align="justify"
@@ -58,37 +53,40 @@ export const Banner = ({ solution, language }: Wso2BladeProps) => {
                             elit.
                         </Typography>
                     </Grid>
-                    </Grid>
-                    <Grid container alignItems="center" justifyContent="center" paddingLeft={2}>
-                        <Box 
-                            component={"img"}
-                            src="https://wso2.cachefly.net/wso2/sites/all/2023/images/webp/wso2-logo.webp"
-                            alt="Healthcare Logo"
-                            sx={{
-                                height: 100,
-                                width: 350,
-                                maxHeight: { xs: 233, md: 167 },
-                                maxWidth: { xs: 350, md: 250 },
-                              }}
-                        >
-                        </Box>
-                    </Grid>
-                </Box>
+                </Grid>
+                <Grid container alignItems="center" justifyContent="center" padding={2}>
+                    <Box
+                        component={"img"}
+                        src="https://wso2.cachefly.net/wso2/sites/all/2023/images/webp/wso2-logo.webp"
+                        alt="Healthcare Logo"
+                        sx={{
+                            height: 100,
+                            width: 350,
+                            maxHeight: { xs: 233, md: 167 },
+                            maxWidth: { xs: 350, md: 250 },
+                        }}
+                    >
+                    </Box>
+                </Grid>
             </Box>
             <Box
                 minHeight={300}
                 alignItems="center"
                 justifyContent="center"
-                display={"flex"}
-                flexDirection={"row"}
-                padding={5}
                 sx={{
+                    minHeight: 300,
                     backgroundColor: "grey.300",
-                    mb: 2,
+                    backdropFilter: "blur(5px)",
+                    flexDirection: "column",
+                    display: "flex",
+                    padding:5,
+
+                    width:"0.5"
+
                 }}
             >
-                <Grid container alignItems="center" justifyContent="center" marginLeft={6}>
-                    <Grid container alignItems="center" justifyContent="center" paddingTop={6}>
+                <Grid container alignItems="center" justifyContent="center" >
+                    <Grid container alignItems="center" justifyContent="center" padding={5}>
                         <Typography variant="h2" align="center" color="#00255C" maxWidth="lg">
                             {language.title}
                         </Typography>
@@ -112,21 +110,21 @@ export const Banner = ({ solution, language }: Wso2BladeProps) => {
                             elit.
                         </Typography>
                     </Grid>
-                    </Grid>
-                    <Grid container alignItems="center" justifyContent="center" padding={2}>
-                        <Box 
-                            component={"img"}
-                            src="https://ballerina.io/images/logo/ballerina-logo-grey.svg"
-                            alt="Healthcare Logo"
-                            sx={{
-                                height: 100,
-                                width: 350,
-                                maxHeight: { xs: 233, md: 167 },
-                                maxWidth: { xs: 350, md: 250 },
-                              }}
-                        >
-                        </Box>
-                    </Grid>
+                </Grid>
+                <Grid container alignItems="center" justifyContent="center" padding={2}>
+                    <Box
+                        component={"img"}
+                        src="https://ballerina.io/images/logo/ballerina-logo-grey.svg"
+                        alt="Healthcare Logo"
+                        sx={{
+                            height: 100,
+                            width: 350,
+                            maxHeight: { xs: 233, md: 167 },
+                            maxWidth: { xs: 350, md: 250 },
+                        }}
+                    >
+                    </Box>
+                </Grid>
             </Box>
         </Box>
     );
