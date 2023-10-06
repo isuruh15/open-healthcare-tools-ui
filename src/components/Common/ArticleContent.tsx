8fca003672d1f2da410ef8c84482e9f1f695dc5b
Thiyanwso2 <65483741+Thiyanwso2@users.noreply.github.com>
2023-10-06 16:13:58 +0530

    Add more responsiveness (#18)

--- src/components/Common/ArticleContent.tsx
+++ src/components/Common/ArticleContent.tsx
@@ -1,11 +1,11 @@
 import {
-    Box,
-    Button,
-    Card,
-    CardMedia,
-    Grid,
-    Stack,
-    Typography
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Grid,
+  Stack,
+  Typography,
 } from "@mui/material";
 
 interface Props {
@@ -17,9 +17,13 @@ interface Props {
 
 function ArticleContent({ imgPath, title, description, link }: Props) {
   return (
-    <Card sx={{ maxWidth: 1200, minWidth: 180 }}>
+    <Card sx={{ maxWidth: 1200, minWidth: 200 }}>
       <Box sx={{ position: "relative" }}>
-        <CardMedia component="img" sx={{minHeight: 250, maxHeight: 350}} image={imgPath} />
+        <CardMedia
+          component="img"
+          sx={{ minHeight: 250, minWidth: 200 }}
+          image={imgPath}
+        />
         <Box
           sx={{
             position: "absolute",
@@ -32,27 +36,49 @@ function ArticleContent({ imgPath, title, description, link }: Props) {
           }}
         >
           <Grid container>
-            <Grid container item xs={6}>
+            <Grid container item xs={10} md={8}>
               <Stack>
                 <Typography variant="h5">{title}</Typography>
                 <Typography variant="body2">{description}</Typography>
               </Stack>
             </Grid>
-            <Grid container item xs={6} justifyContent="center">
+            <Grid
+              container
+              item
+              xs={2}
+              md={4}
+              sx={{
+                justifyContent: "flex-end",
+                paddingRight: { xs: 0, md: 5, lg: 15 },
+              }}
+            >
               <Button
                 href={link}
                 target="_blank"
                 variant="outlined"
                 sx={{
                   borderRadius: 35,
-                  border: "solid 2px #FF7300",
+                  border: { xs: "solid 1px #FF7300", md: "solid 2px #FF7300" },
                   color: "#FFFFFF",
-                  padding: {sm: "0px", md:"1px", lg: "8px"},
-                  width: {sm:100, md: 100, lg:100, xl: 110},
+                  padding: { sm: "0px", md: "1px", lg: "8px" },
+                  width: { sm: 100, md: 100, lg: 100, xl: 110 },
                 }}
               >
-                <Typography justifyContent="center" alignItems="center" variant="body2" sx={{fontSize: {sm: "0.55rem", md: "0.75rem",lg:"0.8rem", xl: "0.857rem"}}}>Read more</Typography>
-                
+                <Typography
+                  justifyContent="center"
+                  alignItems="center"
+                  variant="body2"
+                  sx={{
+                    fontSize: {
+                      sm: "0.60rem",
+                      md: "0.70rem",
+                      lg: "0.8rem",
+                      xl: "0.857rem",
+                    },
+                  }}
+                >
+                  Read more
+                </Typography>
               </Button>
             </Grid>
           </Grid>
--- src/components/Configs/ArticleConfig.tsx
+++ src/components/Configs/ArticleConfig.tsx
@@ -8,20 +8,21 @@ export const articles = [
   },
   {
     title: "FHIR Path evaluator",
-    description: "SHow we implemented FHIR terminology server in Ballerina",
+    description: "How we implemented FHIR terminology",
     imgPath:
       "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=800&h=300&q=60",
     link: "https://google.com",
   },
   {
     title: "EMR connectors",
-    description: "Learn the way to connect to EMR systems using our connectors",
+    description: "Connect to EMR systems",
     imgPath:
       "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=800&h=300",
     link: "https://google.com",
   },
   {
     title: "FHIR APIs",
+    //maximum char count is 30
     description: "Integrate with FHIR APIs",
     imgPath:
       "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=800&h=300&q=60",
