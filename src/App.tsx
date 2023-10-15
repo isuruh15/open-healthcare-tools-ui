90b9e35b753d42eb75a699c8d26f05e67f99c153
Thiyan Arumugam <thiyan@wso2.com>
2023-10-15 12:01:47 +0530

    Create separate theme config constant

--- src/App.tsx
+++ src/App.tsx
@@ -13,77 +13,29 @@ const MainContent = React.lazy(() =>
   }))
 );
 
-let theme = createTheme({
-  typography: {
-    fontFamily: "Plus Jakarta Sans",
-    h1: {
-      fontWeight: 700,
-      fontSize: "2.8rem",
-    },
-    h2:{
-      fontWeight: 700,
-      fontSize: "2rem",
-    },
-    h3: {
-      fontWeight: 700,
-    },
-    h4: {
-      fontWeight: 600,
-    },
-    h5: {
-      fontWeight: 500,
-      fontSize: "1.2rem",
-    },
-  },
-  palette: {
-    primary: {
-      main: "#000000", //Black
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#00A79D", //Asgardeo Green
-      light: "#f7f8fb", // Light grey + purple
-    },
-    text: {
-      primary: "#000000", //Dark blue
-      secondary: "#494848", //Light Grey
-    },
-    background: {
-      paper: "#f7f8fb", // Light grey + purple
-      default: "#ffffff", // White
-    },
-    info: {
-      main: "#00A79D", //Asgardeo Green
-    },
-  },
-});
-theme = responsiveFontSizes(theme);
-
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        <Suspense
-          fallback={
-            <>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100vh",
-                }}
-              >
-                <PreLoader setActive={true} size={50} />
-              </Box>
-            </>
-          }
-        >
-          <MainContent />
-        </Suspense>
-      </Box>
-    </ThemeProvider>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Suspense
+        fallback={
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+              }}
+            >
+              <PreLoader setActive={true} size={50} />
+            </Box>
+          </>
+        }
+      >
+        <MainContent />
+      </Suspense>
+    </Box>
   );
 };
 
