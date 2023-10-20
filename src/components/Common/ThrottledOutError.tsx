06f35c30aa28d551fb03a79e0548f57bd482fc3a
Thiyan Arumugam <thiyan@wso2.com>
2023-10-20 12:37:04 +0530

    Improve throttled out error page

--- src/components/Common/ThrottledOutError.tsx
+++ src/components/Common/ThrottledOutError.tsx
@@ -1,5 +1,8 @@
 import { Box, Grid, Typography } from "@mui/material";
-import { THROTTLED_OUT_PAGE_SUB_TITLE } from "../Configs/TextConstants";
+import {
+  THROTTLED_OUT_PAGE_SUB_TITLE,
+  THROTTLED_OUT_PAGE_TITLE,
+} from "../Configs/TextConstants";
 
 function ThrottledOutError() {
   return (
@@ -16,18 +19,18 @@ function ThrottledOutError() {
         justifyContent="center"
         spacing={5}
         minHeight="100%"
-        padding={8}
+        padding={{ xs: 2, md: 4 }}
       >
-        {/* <Grid item container alignContent="center" justifyContent="center">
-          <Typography
-            variant="h2"
-            textAlign="center"
-          >
-            {THROTTLED_OUT_PAGE_TITLE}
-          </Typography>
-        </Grid> */}
+        <Grid item container alignContent="center" justifyContent="center">
+          <Box component="img" src="throttled.png"></Box>
+        </Grid>
         <Grid item container alignContent="center" justifyContent="center">
           <Typography variant="h4" textAlign="center" border={1} padding={2}>
+            {THROTTLED_OUT_PAGE_TITLE}
+          </Typography>
+        </Grid>
+        <Grid item container alignContent="center" justifyContent="center">
+          <Typography variant="h5" textAlign="center" border={1} padding={2}>
             {THROTTLED_OUT_PAGE_SUB_TITLE}
           </Typography>
         </Grid>
