70e5430bca516144056eba33ad5c1e6a68ee236f
Thiyan Arumugam <thiyan@wso2.com>
2023-11-08 20:39:13 +0530

    Add dynamic redirection after sign in & sign out

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { AuthProvider } from "@asgardeo/auth-react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -6,34 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { theme } from "./components/Configs/ThemeConfig";
 import "./index.css";
-import { BFF_BASE_URL, HL7V2_TO_FHIR_URL } from "./components/Configs/Constants";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const basePath = import.meta.env.VITE_APP_AUTH_REDIRECT_URL;
-
-const config = {
-  signInRedirectURL: basePath,
-  signOutRedirectURL: basePath,
-  clientID: import.meta.env.VITE_APP_AUTH_CLIENT_ID,
-  baseUrl: import.meta.env.VITE_APP_AUTH_BASE_URL,
-  scope: ["openid", "profile"],
-  resourceServerURLs: [BFF_BASE_URL]
-};
-
-console.log("basePath-import", import.meta.env.VITE_APP_AUTH_REDIRECT_URL_TST);
-
 root.render(
-  <AuthProvider config={config}>
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </React.StrictMode>
-  </AuthProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
