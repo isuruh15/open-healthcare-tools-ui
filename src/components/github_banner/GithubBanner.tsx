071949b7cd1451d34c24a35001c4ffee04cd11b2
Thiyan Arumugam <thiyan@wso2.com>
2023-11-23 18:25:31 +0530

    Add powered by Choreo in header

--- src/components/github_banner/GithubBanner.tsx
+++ src/components/github_banner/GithubBanner.tsx
@@ -1,9 +1,7 @@
 import { Box, Button, Grid, Link, Stack, Typography } from "@mui/material";
 import {
-  GITHUB_LINK_LABEL,
   GITHUB_TITLE,
   LANDING_PAGE_BUTTON_LABEL,
-  LANDING_PAGE_SUB_TITLE,
 } from "../../configs/TextConstants";
 
 interface Props {
@@ -22,72 +20,79 @@ function GithubBanner({ marginTop = 3, marginBottom = 2 }: Props) {
       marginBottom={marginBottom}
     >
       <Grid container alignItems="center" justifyContent="center" padding={1}>
-        <Grid item container maxWidth="lg" alignItems="center" justifyContent="center">
-        <Stack spacing={5} paddingBottom={2}>
-          <Typography
-            variant="h2"
-            align="center"
-            color="text.primary"
-            maxWidth="lg"
-          >
-            {GITHUB_TITLE}
-          </Typography>
-          <Typography
-            variant="body1"
-            align="justify"
-            maxWidth="lg"
-            color="text.secondary"
-            lineHeight={{xs: 1.7, md: 1.9}}
-          >
-            This tool transforms HL7v2 messages to FHIR resources. Data
-            transformation conditions are taken from the{" "}
-            <Link
-              href="https://build.fhir.org/ig/HL7/v2-to-fhir/"
-              target="_blank"
-              color="secondary.main"
-              sx={{ textDecoration: "none" }}
-            >
-              official HL7v2 to FHIR mappings guide
-            </Link>{" "}
-            and based on the feedback received from the users. The service is
-            written in Ballerina and hosted in Choreo.{" "}
-            <Box component="span" fontWeight="bold">
-              This tool should not be used in a production environment.
-            </Box>{" "}
-            We do not store any of the data you pasted/uploaded to the tool. If
-            you want to use these services in a production setting, please
-            contact us. For more information, checkout our{" "}
-            <Link
-              href="https://github.com/wso2/open-healthcare-prebuilt-services/tree/main/transformation/v2-to-fhirr4-service"
-              target="_blank"
-              color="secondary.main"
-              sx={{ textDecoration: "none" }}
-            >
-              Git repository
-            </Link>
-            .
-          </Typography>
-        </Stack>
-        </Grid>
-
-      </Grid>
-      <Grid item container alignItems="center" justifyContent="center">
-      <Button
-          variant="contained"
-          color="info"
-          href="https://wso2.com/contact/?ref=Healthcare"
-          target="_blank"
-          size="large"
-          sx={{
-            padding: {xs: "5px 28px", sm: "6px, 32px", md:"8px 36px"},
-            color: "#fff",
-            fontWeight: {xs:550, sm: 600},
-            fontSize: {xs:"1rem", small:"1.1rem", md: "1.2rem"},
-            alignSelf: "center",
-          }}
+        <Grid
+          item
+          container
+          alignItems="center"
+          justifyContent="center"
+          maxWidth="xl"
         >
-          {LANDING_PAGE_BUTTON_LABEL}
-        </Button>
+          <Grid item container alignItems="center" justifyContent="center">
+            <Stack spacing={5} paddingBottom={2}>
+              <Typography
+                variant="h2"
+                align="center"
+                color="text.primary"
+                maxWidth="lg"
+              >
+                {GITHUB_TITLE}
+              </Typography>
+              <Typography
+                variant="body1"
+                align="justify"
+                maxWidth="lg"
+                color="text.secondary"
+                lineHeight={{ xs: 1.7, md: 1.9 }}
+              >
+                This tool transforms HL7v2 messages to FHIR resources. Data
+                transformation conditions are taken from the{" "}
+                <Link
+                  href="https://build.fhir.org/ig/HL7/v2-to-fhir/"
+                  target="_blank"
+                  color="secondary.main"
+                  sx={{ textDecoration: "none" }}
+                >
+                  official HL7v2 to FHIR mappings guide
+                </Link>{" "}
+                and based on the feedback received from the users. The service
+                is written in Ballerina and hosted in Choreo.{" "}
+                <Box component="span" fontWeight="bold">
+                  This tool should not be used in a production environment.
+                </Box>{" "}
+                We do not store any of the data you pasted/uploaded to the tool.
+                If you want to use these services in a production setting,
+                please contact us. For more information, checkout our{" "}
+                <Link
+                  href="https://github.com/wso2/open-healthcare-prebuilt-services/tree/main/transformation/v2-to-fhirr4-service"
+                  target="_blank"
+                  color="secondary.main"
+                  sx={{ textDecoration: "none" }}
+                >
+                  Git repository
+                </Link>
+                .
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid item container alignItems="center" justifyContent="center">
+            <Button
+              variant="contained"
+              color="info"
+              href="https://wso2.com/contact/?ref=Healthcare"
+              target="_blank"
+              size="large"
+              sx={{
+                padding: { xs: "5px 28px", sm: "6px, 32px", md: "8px 36px" },
+                color: "#fff",
+                fontWeight: { xs: 550, sm: 600 },
+                fontSize: { xs: "1rem", small: "1.1rem", md: "1.2rem" },
+                alignSelf: "center",
+              }}
+            >
+              {LANDING_PAGE_BUTTON_LABEL}
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
     </Box>
   );
--- src/components/header/ComponentTitle.tsx
+++ src/components/header/ComponentTitle.tsx
@@ -1,40 +1,84 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
+import { POWERED_BY_LABEL } from "../../configs/TextConstants";
 
 interface Props {
   heading: string;
   description: string;
-  url: string;
 }
 
-export const ComponentTitle = ({ heading, description, url }: Props) => {
+export const ComponentTitle = ({ heading, description }: Props) => {
   return (
     <>
       <Box
-        sx={{ display: "flex", flexDirection: "column", mt: 2, ml: 1, mb: 1 }}
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          mt: 2,
+          mb: 1,
+        }}
       >
-        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-          <Typography
-            variant="h5"
-            sx={{
-              fontWeight: 600,
-            }}
-            id="component-heading"
-            aria-label="Component heading"
-            color="text.primary"
-          >
-            {heading}
-          </Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", ml: 1 }}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 600,
+              }}
+              id="component-heading"
+              aria-label="Component heading"
+              color="text.primary"
+            >
+              {heading}
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", gap: 0.5 }}>
+            <Typography
+              color="text.secondary"
+              variant="body2"
+              sx={{ fontWeight: 400 }}
+              id="component-description"
+              aria-label="Component description"
+            >
+              {description}
+            </Typography>
+          </Box>
         </Box>
-        <Box sx={{ display: "flex", gap: 0.5 }}>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 0.8,
+            ml: { xs: 1, md: 2 },
+            mt: { xs: 1, sm: 0 },
+            borderLeft: { xs: 0, md: 1 },
+          }}
+        >
           <Typography
-            color="text.secondary"
-            variant="body1"
-            sx={{ fontWeight: 400 }}
-            id="component-description"
-            aria-label="Component description"
+            variant="body2"
+            sx={{
+              color: "text.primary",
+              alignItems: "center",
+              display: "flex",
+              ml: { xs: 0, md: 1.2 },
+            }}
+            id="footer-text"
           >
-            {description}
+            {POWERED_BY_LABEL}
           </Typography>
+          <Link
+            href="https://wso2.com/choreo/"
+            target="_blank"
+            paddingTop={1.5}
+          >
+            <Box
+              component={"img"}
+              src="wso2-related-logo/choreo.svg"
+              alt="Choreo Logo"
+              sx={{
+                width: { xs: 80, sm: 80, md: 80, lg: 80 },
+                height: { xs: 25, sm: 25, md: 25, lg: 25 },
+              }}
+            />
+          </Link>
         </Box>
       </Box>
     </>
